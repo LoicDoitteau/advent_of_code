845cd8b2b1f63e4cbe9f3cbbbf0be144cd206efa
LoicDoitteau <loic.doitteau@viacesi.fr>
2018-12-16 14:52:54 +0100

    Day 16

--- 2018/day_16/part_2/contest.js
+++ 2018/day_16/part_2/contest.js
@@ -1,10 +1,10 @@
 fs = require("fs");
 
-fs.readFile("./2018/day_16/part_1/input.txt", 'utf8', (err, input) => {
+fs.readFile("./2018/day_16/part_2/input.txt", 'utf8', (err, input) => {
     var p = /Before: \[(\d+), (\d+), (\d+), (\d+)\]\r\n(\d+) (\d+) (\d+) (\d+)\r\nAfter:  \[(\d+), (\d+), (\d+), (\d+)\]/gm;
-    var r = 0;
     var m;
     var register;
+    var assumptions = {};
     var instructions = {
         addr : (input1, input2, output) => register[output] = register[input1] + register[input2],
         addi : (input1, input2, output) => register[output] = register[input1] + input2,
@@ -27,11 +27,32 @@ fs.readFile("./2018/day_16/part_1/input.txt", 'utf8', (err, input) => {
         var [, rb1, rb2, rb3, rb4, opcode, input1, input2, output, ra1, ra2, ra3, ra4] = m.map(Number);
         var registerBefore = [rb1, rb2, rb3, rb4];
         var registerAfter = [ra1, ra2, ra3, ra4];
-        if(Object.keys(instructions).filter(instr => {
+        var assumption = [];
+        Object.keys(instructions).forEach(instr => {
             register = registerBefore.slice();
             instructions[instr](input1, input2, output);
-            return register.every((v, i) => v == registerAfter[i]);
-        }).length >= 3) r++;
+            if(register.every((v, i) => v == registerAfter[i])) assumption.push(instr);
+        });
+        if(assumptions[opcode] == undefined) assumptions[opcode] = assumption;
+        else assumptions[opcode] = assumptions[opcode].filter(instr => assumption.indexOf(instr) != -1);
     }
-    console.log(r);
+    while(Object.keys(assumptions).some(opcode => assumptions[opcode].length > 1)) {
+        var done = Object.keys(assumptions).reduce((acc, opcode) => {
+            if(assumptions[opcode].length == 1) acc.push(assumptions[opcode][0]);
+            return acc;
+        }, []);
+        Object.keys(assumptions).forEach(opcode => {
+            if(assumptions[opcode].length > 1) assumptions[opcode] = assumptions[opcode].filter(instr => done.indexOf(instr) == -1);
+        });
+    }
+    fs.readFile("./2018/day_16/part_2/input2.txt", 'utf8', (err2, prog) => {
+        p = /(\d+) (\d+) (\d+) (\d+)/g;
+        register = [0, 0, 0, 0];
+        while((m = p.exec(prog)) != null) {
+            var [, opcode, input1, input2, output] = m.map(Number);
+            var instr = assumptions[opcode][0];
+            instructions[instr](input1, input2, output);
+        }
+        console.log(register);
+    });
 });
