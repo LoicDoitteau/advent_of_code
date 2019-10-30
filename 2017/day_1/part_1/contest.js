f16ad3481db11de4b1ee2e5dde9a51a2353de734
DOITTEAU Loïc <loic.doitteau@econocom.com>
2019-10-30 14:10:53 +0100

    2018 Day 1

---
+++ 2017/day_1/part_1/contest.js
@@ -0,0 +1,7 @@
+fs = require("fs");
+fs.readFile("./2017/day_1/part_1/input.txt", 'utf8', (err, input) => {
+    var r = Array
+        .from(input)  
+        .reduce((acc, x, i) => x == input[(i+1) % input.length] ? acc + Number(x) : acc, 0);
+    console.log(r);
+});
