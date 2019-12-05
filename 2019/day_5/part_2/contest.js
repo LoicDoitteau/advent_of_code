62fbbb9f6b745968450d71f981c66d0e004dbe60
DOITTEAU Loïc <loic.doitteau@econocom.com>
2019-12-05 13:49:22 +0100

    2019 day 5 part 2

---
+++ 2019/day_5/part_2/contest.js
@@ -0,0 +1,77 @@
+fs = require("fs");
+fs.readFile("./2019/day_5/input.txt", 'utf8', (err, data) => {
+    let intcode = data.split(',').map(Number);
+    let pointer = 0;
+    let input = 5;
+    let output = 0;
+
+    const modes = {
+        "0" : value => intcode[value],
+        "1" : value => value
+    };
+
+    const instructions = {
+        "01" : parametersMode => {
+            const value1 = modes[parametersMode[1]](intcode[pointer + 1]);
+            const value2 = modes[parametersMode[0]](intcode[pointer + 2]);
+            const value3 = intcode[pointer + 3];
+            intcode[value3] = value1 + value2;
+            pointer += 4;
+        },
+        "02" : parametersMode => {
+            const value1 = modes[parametersMode[1]](intcode[pointer + 1]);
+            const value2 = modes[parametersMode[0]](intcode[pointer + 2]);
+            const value3 = intcode[pointer + 3];
+            intcode[value3] = value1 * value2;
+            pointer += 4;
+        },
+        "03" : () => {
+            const value1 = intcode[pointer + 1];
+            intcode[value1] = input;
+            pointer += 2;
+        },
+        "04" : () => {
+            const value1 = intcode[pointer + 1];
+            output = intcode[value1]
+            pointer += 2;
+        },
+        "05" : parametersMode => {
+            const value1 = modes[parametersMode[1]](intcode[pointer + 1]);
+            const value2 = modes[parametersMode[0]](intcode[pointer + 2]);
+            if(value1 != 0) pointer = value2;
+            else pointer += 3;
+        },
+        "06" : parametersMode => {
+            const value1 = modes[parametersMode[1]](intcode[pointer + 1]);
+            const value2 = modes[parametersMode[0]](intcode[pointer + 2]);
+            if(value1 == 0) pointer = value2;
+            else pointer += 3;
+        },
+        "07" : parametersMode => {
+            const value1 = modes[parametersMode[1]](intcode[pointer + 1]);
+            const value2 = modes[parametersMode[0]](intcode[pointer + 2]);
+            const value3 = intcode[pointer + 3];
+            intcode[value3] = value1 < value2 ? 1 : 0;
+            pointer += 4;
+        },
+        "08" : parametersMode => {
+            const value1 = modes[parametersMode[1]](intcode[pointer + 1]);
+            const value2 = modes[parametersMode[0]](intcode[pointer + 2]);
+            const value3 = intcode[pointer + 3];
+            intcode[value3] = value1 == value2 ? 1 : 0;
+            pointer += 4;
+        }
+    };
+
+
+    while(true) {
+        const program = intcode[pointer].toString().padStart(4, '0');
+        let parameters = program.slice(0, program.length - 2);
+        let opcode = program.slice(program.length - 2);
+
+        if(opcode == "99") break;
+        instructions[opcode](parameters);
+    }
+
+    console.log(output);
+});
