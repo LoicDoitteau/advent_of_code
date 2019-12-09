265bb21d2ec395699b3cf2269316f8cb84eabdc2
DOITTEAU Loïc <loic.doitteau@econocom.com>
2019-12-09 14:08:46 +0100

    Clean

--- 2019/day_9/part_1&2/contest.js
+++ 2019/day_9/part_1&2/contest.js
@@ -4,17 +4,17 @@ fs.readFile("./2019/day_9/input.txt", 'utf8', (err, data) => {
     let intcode = data.split(',').map(Number);
     let pointer = 0;
     let relativeBase = 0;
-    let input = 1;
+    let input = 2;
     let output = 0;
 
     const modes = {
-        input : {
+        read : {
             "0" : value => intcode[value] || 0,
             "1" : value => value,
             "2" : (value, offset) => intcode[value + offset] || 0
         },
         
-        output : {
+        write : {
             "0" : value => value,
             "2" : (value, offset) => value + offset
         }
@@ -22,57 +22,57 @@ fs.readFile("./2019/day_9/input.txt", 'utf8', (err, data) => {
 
     const instructions = {
         "01" : parametersMode => {
-            const value1 = modes.input[parametersMode[2]](intcode[pointer + 1] || 0, relativeBase);
-            const value2 = modes.input[parametersMode[1]](intcode[pointer + 2] || 0, relativeBase);
-            const value3 = modes.output[parametersMode[0]](intcode[pointer + 3] || 0, relativeBase);
+            const value1 = modes.read[parametersMode[2]](intcode[pointer + 1] || 0, relativeBase);
+            const value2 = modes.read[parametersMode[1]](intcode[pointer + 2] || 0, relativeBase);
+            const value3 = modes.write[parametersMode[0]](intcode[pointer + 3] || 0, relativeBase);
             intcode[value3] = value1 + value2;
             pointer += 4;
         },
         "02" : parametersMode => {
-            const value1 = modes.input[parametersMode[2]](intcode[pointer + 1] || 0, relativeBase);
-            const value2 = modes.input[parametersMode[1]](intcode[pointer + 2] || 0, relativeBase);
-            const value3 = modes.output[parametersMode[0]](intcode[pointer + 3] || 0, relativeBase);
+            const value1 = modes.read[parametersMode[2]](intcode[pointer + 1] || 0, relativeBase);
+            const value2 = modes.read[parametersMode[1]](intcode[pointer + 2] || 0, relativeBase);
+            const value3 = modes.write[parametersMode[0]](intcode[pointer + 3] || 0, relativeBase);
             intcode[value3] = value1 * value2;
             pointer += 4;
         },
         "03" : parametersMode => {
-            const value1 = modes.output[parametersMode[2]](intcode[pointer + 1] || 0, relativeBase);
+            const value1 = modes.write[parametersMode[2]](intcode[pointer + 1] || 0, relativeBase);
             intcode[value1] = input;
             pointer += 2;
         },
         "04" : parametersMode => {
-            const value1 = modes.input[parametersMode[2]](intcode[pointer + 1] || 0, relativeBase);
+            const value1 = modes.read[parametersMode[2]](intcode[pointer + 1] || 0, relativeBase);
             output = value1;
             pointer += 2;
         },
         "05" : parametersMode => {
-            const value1 = modes.input[parametersMode[2]](intcode[pointer + 1] || 0, relativeBase);
-            const value2 = modes.input[parametersMode[1]](intcode[pointer + 2] || 0, relativeBase);
+            const value1 = modes.read[parametersMode[2]](intcode[pointer + 1] || 0, relativeBase);
+            const value2 = modes.read[parametersMode[1]](intcode[pointer + 2] || 0, relativeBase);
             if(value1 != 0) pointer = value2;
             else pointer += 3;
         },
         "06" : parametersMode => {
-            const value1 = modes.input[parametersMode[2]](intcode[pointer + 1] || 0, relativeBase);
-            const value2 = modes.input[parametersMode[1]](intcode[pointer + 2] || 0, relativeBase);
+            const value1 = modes.read[parametersMode[2]](intcode[pointer + 1] || 0, relativeBase);
+            const value2 = modes.read[parametersMode[1]](intcode[pointer + 2] || 0, relativeBase);
             if(value1 == 0) pointer = value2;
             else pointer += 3;
         },
         "07" : parametersMode => {
-            const value1 = modes.input[parametersMode[2]](intcode[pointer + 1] || 0, relativeBase);
-            const value2 = modes.input[parametersMode[1]](intcode[pointer + 2] || 0, relativeBase);
-            const value3 = modes.output[parametersMode[0]](intcode[pointer + 3] || 0, relativeBase);
+            const value1 = modes.read[parametersMode[2]](intcode[pointer + 1] || 0, relativeBase);
+            const value2 = modes.read[parametersMode[1]](intcode[pointer + 2] || 0, relativeBase);
+            const value3 = modes.write[parametersMode[0]](intcode[pointer + 3] || 0, relativeBase);
             intcode[value3] = value1 < value2 ? 1 : 0;
             pointer += 4;
         },
         "08" : parametersMode => {
-            const value1 = modes.input[parametersMode[2]](intcode[pointer + 1] || 0, relativeBase);
-            const value2 = modes.input[parametersMode[1]](intcode[pointer + 2] || 0, relativeBase);
-            const value3 = modes.output[parametersMode[0]](intcode[pointer + 3] || 0, relativeBase);
+            const value1 = modes.read[parametersMode[2]](intcode[pointer + 1] || 0, relativeBase);
+            const value2 = modes.read[parametersMode[1]](intcode[pointer + 2] || 0, relativeBase);
+            const value3 = modes.write[parametersMode[0]](intcode[pointer + 3] || 0, relativeBase);
             intcode[value3] = value1 == value2 ? 1 : 0;
             pointer += 4;
         },
         "09" : parametersMode => {
-            const value1 = modes.input[parametersMode[2]](intcode[pointer + 1] || 0, relativeBase);
+            const value1 = modes.read[parametersMode[2]](intcode[pointer + 1] || 0, relativeBase);
             relativeBase += value1;
             pointer += 2;
         }
