f9c4f9ad9117ffeb585f14ea2d945093a1908888
DOITTEAU Loïc <loic.doitteau@econocom.com>
2019-12-13 12:13:42 +0100

    2019 day 13 part 2

---
+++ 2019/day_13/part_2/contest.js
@@ -0,0 +1,155 @@
+fs = require("fs");
+fs.readFile("./2019/day_13/input.txt", 'utf8', (err, data) => {
+    const startTime = new Date().getTime();
+    const states = {RUNNING : 0, HALTED : 1, STOPPED : 2};
+    const tiles = {EMPTY : 0, WALL : 1, BLOCK : 2, PADDLE : 3, BALL : 4};
+    const position = {NEUTRAL : 0, LEFT : -1, RIGHT : 1};
+    const DEBUG = false;
+    let res = null;
+
+    function IntProgram() {
+        this.pointer = 0;
+        this.relativeBase = 0;
+        this.intcode = data.split(',').map(Number);
+        this.input = null;
+        this.output = null;
+        this.state = states.RUNNING;
+
+        this.modes = {
+            read : {
+                "0" : value => this.intcode[value] || 0,
+                "1" : value => value,
+                "2" : (value, offset) => this.intcode[value + offset] || 0
+            },
+            
+            write : {
+                "0" : value => value,
+                "2" : (value, offset) => value + offset
+            }
+        };
+
+        this.instructions = {
+            "01" : parametersMode => {
+                const value1 = this.modes.read[parametersMode[2]](this.intcode[this.pointer + 1] || 0, this.relativeBase);
+                const value2 = this.modes.read[parametersMode[1]](this.intcode[this.pointer + 2] || 0, this.relativeBase);
+                const value3 = this.modes.write[parametersMode[0]](this.intcode[this.pointer + 3] || 0, this.relativeBase);
+                this.intcode[value3] = value1 + value2;
+                this.pointer += 4;
+            },
+            "02" : parametersMode => {
+                const value1 = this.modes.read[parametersMode[2]](this.intcode[this.pointer + 1] || 0, this.relativeBase);
+                const value2 = this.modes.read[parametersMode[1]](this.intcode[this.pointer + 2] || 0, this.relativeBase);
+                const value3 = this.modes.write[parametersMode[0]](this.intcode[this.pointer + 3] || 0, this.relativeBase);
+                this.intcode[value3] = value1 * value2;
+                this.pointer += 4;
+            },
+            "03" : parametersMode => {
+                const value1 = this.modes.write[parametersMode[2]](this.intcode[this.pointer + 1] || 0, this.relativeBase);
+                this.intcode[value1] = this.input;
+                this.pointer += 2;
+            },
+            "04" : parametersMode => {
+                const value1 = this.modes.read[parametersMode[2]](this.intcode[this.pointer + 1] || 0, this.relativeBase);
+                this.output = value1;
+                this.pointer += 2;
+                this.state = states.HALTED;
+            },
+            "05" : parametersMode => {
+                const value1 = this.modes.read[parametersMode[2]](this.intcode[this.pointer + 1] || 0, this.relativeBase);
+                const value2 = this.modes.read[parametersMode[1]](this.intcode[this.pointer + 2] || 0, this.relativeBase);
+                if(value1 != 0) this.pointer = value2;
+                else this.pointer += 3;
+            },
+            "06" : parametersMode => {
+                const value1 = this.modes.read[parametersMode[2]](this.intcode[this.pointer + 1] || 0, this.relativeBase);
+                const value2 = this.modes.read[parametersMode[1]](this.intcode[this.pointer + 2] || 0, this.relativeBase);
+                if(value1 == 0) this.pointer = value2;
+                else this.pointer += 3;
+            },
+            "07" : parametersMode => {
+                const value1 = this.modes.read[parametersMode[2]](this.intcode[this.pointer + 1] || 0, this.relativeBase);
+                const value2 = this.modes.read[parametersMode[1]](this.intcode[this.pointer + 2] || 0, this.relativeBase);
+                const value3 = this.modes.write[parametersMode[0]](this.intcode[this.pointer + 3] || 0, this.relativeBase);
+                this.intcode[value3] = value1 < value2 ? 1 : 0;
+                this.pointer += 4;
+            },
+            "08" : parametersMode => {
+                const value1 = this.modes.read[parametersMode[2]](this.intcode[this.pointer + 1] || 0, this.relativeBase);
+                const value2 = this.modes.read[parametersMode[1]](this.intcode[this.pointer + 2] || 0, this.relativeBase);
+                const value3 = this.modes.write[parametersMode[0]](this.intcode[this.pointer + 3] || 0, this.relativeBase);
+                this.intcode[value3] = value1 == value2 ? 1 : 0;
+                this.pointer += 4;
+            },
+            "09" : parametersMode => {
+                const value1 = this.modes.read[parametersMode[2]](this.intcode[this.pointer + 1] || 0, this.relativeBase);
+                this.relativeBase += value1;
+                this.pointer += 2;
+            },
+            "99" : () => {
+                this.state = states.STOPPED;
+            }
+        };
+
+        return {
+            memory : this.intcode,
+            process : value => {
+                this.input = value;
+                this.state = states.RUNNING;
+                while(true) {
+                    const program = this.intcode[this.pointer].toString().padStart(5, '0');
+                    let parameters = program.slice(0, program.length - 2);
+                    let opcode = program.slice(program.length - 2);
+
+                    if(this.state != states.RUNNING) return {state : this.state, output : this.output};
+                    this.instructions[opcode](parameters);
+                }
+            }
+        }
+    }
+
+    const screen = {};
+    const prog = new IntProgram();
+    prog.memory[0] = 2;
+
+    let paddle = null;
+    let ball = null;
+    let score = null;
+    let input = position.NEUTRAL;
+
+    while(true) {
+        let state = prog.process(input);
+        if(state.state == states.STOPPED) break;
+        const x = state.output;
+
+        state = prog.process(input);
+        if(state.state == states.STOPPED) break;
+        const y = state.output;
+
+        state = prog.process(input);
+        if(state.state == states.STOPPED) break;
+        const id = state.output;
+
+        if(!screen[x]) screen[x] = {};
+        screen[x][y] = id;
+
+        if(x == -1 && y == 0) score = id;
+        else if(id == tiles.PADDLE) paddle = {x, y};
+        else if(id == tiles.BALL) ball = {x, y};
+
+        if(paddle && ball) {
+            if(ball.x < paddle.x) input = position.LEFT;
+            else if(ball.x > paddle.x) input = position.RIGHT;
+            else input = position.NEUTRAL;
+        }
+        
+        if(DEBUG) {
+            const countBlocks = Object.keys(screen).reduce((acc, x) => acc + Object.keys(screen[x]).filter(y => screen[x][y] == tiles.BLOCK).length, 0);
+            console.log(countBlocks);
+        }
+    }
+
+    res = score;
+    console.log(res);
+    const elapsedTime = new Date().getTime() - startTime;
+    console.log(`Elapsed : ${elapsedTime} ms`);
+});
