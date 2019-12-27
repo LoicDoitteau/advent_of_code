782cdb694a0dedbf6b67cec26c384da168294f7c
LoicDoitteau <loic.doitteau@viacesi.fr>
2019-12-27 14:24:19 +0100

    2019 day 21 part 2

---
+++ 2019/day_21/part_2/contest.js
@@ -0,0 +1,148 @@
+fs = require("fs");
+fs.readFile("./2019/day_21/input.txt", 'utf8', (err, data) => {
+    const startTime = new Date().getTime();
+    const states = {RUNNING : 0, HALTED : 1, STOPPED : 2};
+    const DEBUG = true;
+    let res = null;
+
+    function IntProgram() {
+        this.pointer = 0;
+        this.relativeBase = 0;
+        this.intcode = data.split(',').map(Number);
+        this.input = null;
+        this.output = null;
+        this.state = states.RUNNING;
+        this.inputIndex = 0;
+
+        this.modes = {
+            read : {
+                "0" : value => this.intcode[value] || 0,
+                "1" : value => value,
+                "2" : (value, offset) => this.intcode[value + offset] || 0
+            },
+            
+            write : {
+                "0" : value => value,
+                "2" : (value, offset) => value + offset
+            }
+        };
+
+        this.instructions = {
+            "01" : parametersMode => {
+                const value1 = this.modes.read[parametersMode[2]](this.intcode[this.pointer + 1] || 0, this.relativeBase);
+                const value2 = this.modes.read[parametersMode[1]](this.intcode[this.pointer + 2] || 0, this.relativeBase);
+                const value3 = this.modes.write[parametersMode[0]](this.intcode[this.pointer + 3] || 0, this.relativeBase);
+                this.intcode[value3] = value1 + value2;
+                this.pointer += 4;
+            },
+            "02" : parametersMode => {
+                const value1 = this.modes.read[parametersMode[2]](this.intcode[this.pointer + 1] || 0, this.relativeBase);
+                const value2 = this.modes.read[parametersMode[1]](this.intcode[this.pointer + 2] || 0, this.relativeBase);
+                const value3 = this.modes.write[parametersMode[0]](this.intcode[this.pointer + 3] || 0, this.relativeBase);
+                this.intcode[value3] = value1 * value2;
+                this.pointer += 4;
+            },
+            "03" : parametersMode => {
+                const value1 = this.modes.write[parametersMode[2]](this.intcode[this.pointer + 1] || 0, this.relativeBase);
+                this.intcode[value1] = this.input[this.inputIndex % this.input.length];
+                this.inputIndex++;
+                this.pointer += 2;
+            },
+            "04" : parametersMode => {
+                const value1 = this.modes.read[parametersMode[2]](this.intcode[this.pointer + 1] || 0, this.relativeBase);
+                this.output = value1;
+                this.pointer += 2;
+                this.state = states.HALTED;
+            },
+            "05" : parametersMode => {
+                const value1 = this.modes.read[parametersMode[2]](this.intcode[this.pointer + 1] || 0, this.relativeBase);
+                const value2 = this.modes.read[parametersMode[1]](this.intcode[this.pointer + 2] || 0, this.relativeBase);
+                if(value1 != 0) this.pointer = value2;
+                else this.pointer += 3;
+            },
+            "06" : parametersMode => {
+                const value1 = this.modes.read[parametersMode[2]](this.intcode[this.pointer + 1] || 0, this.relativeBase);
+                const value2 = this.modes.read[parametersMode[1]](this.intcode[this.pointer + 2] || 0, this.relativeBase);
+                if(value1 == 0) this.pointer = value2;
+                else this.pointer += 3;
+            },
+            "07" : parametersMode => {
+                const value1 = this.modes.read[parametersMode[2]](this.intcode[this.pointer + 1] || 0, this.relativeBase);
+                const value2 = this.modes.read[parametersMode[1]](this.intcode[this.pointer + 2] || 0, this.relativeBase);
+                const value3 = this.modes.write[parametersMode[0]](this.intcode[this.pointer + 3] || 0, this.relativeBase);
+                this.intcode[value3] = value1 < value2 ? 1 : 0;
+                this.pointer += 4;
+            },
+            "08" : parametersMode => {
+                const value1 = this.modes.read[parametersMode[2]](this.intcode[this.pointer + 1] || 0, this.relativeBase);
+                const value2 = this.modes.read[parametersMode[1]](this.intcode[this.pointer + 2] || 0, this.relativeBase);
+                const value3 = this.modes.write[parametersMode[0]](this.intcode[this.pointer + 3] || 0, this.relativeBase);
+                this.intcode[value3] = value1 == value2 ? 1 : 0;
+                this.pointer += 4;
+            },
+            "09" : parametersMode => {
+                const value1 = this.modes.read[parametersMode[2]](this.intcode[this.pointer + 1] || 0, this.relativeBase);
+                this.relativeBase += value1;
+                this.pointer += 2;
+            },
+            "99" : () => {
+                this.state = states.STOPPED;
+            }
+        };
+
+        return {
+            memory : this.intcode,
+            process : (value) => {
+                this.input = value;
+                this.state = states.RUNNING;
+                while(true) {
+                    const program = this.intcode[this.pointer].toString().padStart(5, '0');
+                    let parameters = program.slice(0, program.length - 2);
+                    let opcode = program.slice(program.length - 2);
+
+                    if(this.state != states.RUNNING) return {state : this.state, output : this.output, index : this.inputIndex};
+                    this.instructions[opcode](parameters);
+                }
+            }
+        }
+    }
+
+    const springScriptProgram = () => 
+    `NOT A J
+    NOT B T
+    OR T J
+    NOT C T
+    OR T J
+    AND D J
+    NOT D T
+    OR E T
+    OR H T
+    AND T J
+    RUN
+    `;
+
+    const encode = s => Array.prototype.map.call(s, c => c.charCodeAt(0));
+    const decode = c => String.fromCharCode(...c);
+
+    const run = () => {
+        const prog = new IntProgram();
+        const codes = [];
+        let state = null;
+
+        const input = encode(springScriptProgram());
+        while(true){
+            state = prog.process(input);
+            if(state.state == states.STOPPED) {
+                const s = decode(codes);
+                if(DEBUG) console.log(s);
+                return state.output;
+            }
+            codes.push(state.output);
+        }
+    }
+  
+    res = run();
+    console.log(res);
+    const elapsedTime = new Date().getTime() - startTime;
+    console.log(`Elapsed : ${elapsedTime} ms`);
+});
