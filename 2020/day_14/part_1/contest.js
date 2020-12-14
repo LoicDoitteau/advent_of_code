a16b883f80bf4b3e1a0574c87ad2a82d71efafab
Loïc Doitteau <loic.doitteau@gmail.com>
2020-12-14 11:09:02 +0100

    2020 Day 14 Part 1

---
+++ 2020/day_14/part_1/contest.js
@@ -0,0 +1,44 @@
+const { count } = require("console");
+
+fs = require("fs");
+fs.readFile("./2020/day_14/input.txt", 'utf8', (err, input) => {
+
+    const instructions = input.split("\r\n");
+    const maskRegex = /mask = (?<mask>(?:0|1|X){36})/;
+    const valueRegex = /mem\[(?<address>\d+)\] = (?<value>\d+)/;
+    let currentMask = [];
+    const memory = {}
+
+    const applyMask = (mask, value) => {
+        let valueBin = value.toString(2).padStart(36, '0').split('');
+        for (const bit of mask) {
+            valueBin[bit.index] = bit.value;
+        }
+
+        let newValue = 0n;
+        for (let i = 0n; i < valueBin.length; i++) {
+            newValue += BigInt(valueBin[i]) * (2n ** (35n - i));
+        }
+        return newValue;
+    }
+
+    for (const instruction of instructions) {
+        if (maskRegex.test(instruction)) {
+            currentMask = maskRegex.exec(instruction)
+                .groups
+                .mask
+                .split('')
+                .reduce((acc, value, index) => {
+                    if (value != 'X')
+                        acc.push({ value, index });
+                    return acc;
+                }, []);
+        } else {
+            const { address, value } = /mem\[(?<address>\d+)\] = (?<value>\d+)/.exec(instruction).groups;
+            memory[address] = applyMask(currentMask, BigInt(value));
+        }
+    }
+
+    const response = Object.keys(memory).reduce((acc, address) => acc + memory[address], 0n);
+    console.log(response);
+});
